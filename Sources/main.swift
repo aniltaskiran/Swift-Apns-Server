bd0780fae6b503126ce4d40a63a428eacbae09e3
Anıl T <kev@Anl-MacBook-Pro.local>
2018-01-14 01:23:54 +0300

    repeater with web server

--- Sources/main.swift
+++ Sources/main.swift
@@ -6,34 +6,15 @@ import PerfectNotifications
 import PerfectRepeater
 // your app id. we use this as the configuration name, but they do not have to match
 
-
-
-
-
-var opt = 1
-
 let c = {
     () -> Bool in
-    print("XXXXXX")
+    print("loop")
     writeValue(Query: "INSERT INTO Deneme(date) values(NOW())")
     writeValue(Query: "INSERT INTO Devices(token) values(\"merhaba\")")
     return true
 }
 
-let cc = {
-    () -> Bool in
-    print("Hello, world! (\(opt))")
-    if opt < 10 {
-        opt += 1
-        return true
-    } else {
-        print("cc exiting.")
-        return false
-    }
-}
-
 Repeater.exec(timer: 3.0, callback: c)
-Repeater.exec(timer: 2.0, callback: cc)
 
 var shouldKeepRunning = true        // change this `false` to stop the application from running
 let theRL = RunLoop.current         // Need a reference to the current run loop
@@ -85,10 +66,10 @@ let confData = [
     ]
 ]
 
-//do {
-//    // Launch the servers based on the configuration data.
-//    try HTTPServer.launch(configurationData: confData)
-//} catch {
-//    fatalError("\(error)") // fatal error launching one of the servers
-//}
+do {
+    // Launch the servers based on the configuration data.
+    try HTTPServer.launch(configurationData: confData)
+} catch {
+    fatalError("\(error)") // fatal error launching one of the servers
+}
 
