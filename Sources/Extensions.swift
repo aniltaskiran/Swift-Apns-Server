26fe8211b2e025552378cddffec2c22d828dae03
Anıl T <kev@Anl-MacBook-Pro.local>
2018-01-14 05:19:30 +0300

    utf8

--- Sources/Extensions.swift
+++ Sources/Extensions.swift
@@ -6,10 +6,18 @@
 //
 
 import Foundation
+extension String {
+func utf8DecodedString()-> String {
+    let data = self.data(using: .utf8)
+    if let message = String(data: data!, encoding: .nonLossyASCII){
+        return message
+    }
+    return ""
+}
 
-//extension Array where Element: String {
-//    func toString() -> String {
-//        return "[\(self.joined(separator: ","))]"
-//    }
-//}
-
+func utf8EncodedString()-> String {
+    let messageData = self.data(using: .nonLossyASCII)
+    let text = String(data: messageData!, encoding: .utf8)
+    return text!
+}
+}
