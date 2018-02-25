2dd423f8499113d481a1202ab1bbce2104a90a6e
Anıl Taşkıran <aniltaskiran@outlook.com.tr>
2018-02-25 12:30:43 +0300

    rich push geliştirildi

--- Sources/Device.swift
+++ Sources/Device.swift
@@ -14,20 +14,23 @@ public class Device {
     static let instance = Device()
 
     func notify(title: String, message: String, deviceTokens: [String], isSilent: Bool) {
-        var notItems: [APNSNotificationItem] = [.alertTitle(title), .alertBody(message), .sound("default")]
+        let data = ["attachment-url":"https://github.com/Sweefties/WatchOS2-NewUI-Movie-Example/raw/master/WatchOS2-NewUI-Movie-Example%20WatchKit%20App/MoviePath/burningmanbyair.m4v"]
+        
+        var notItems: [APNSNotificationItem] = [.alertTitle(title), .alertBody(message), .sound("default"), .mutableContent, .category("videoIdentifier"), .customPayload("data", data)]
+        
         if isSilent {
             print("silent gönderildi.")
             notItems.removeAll()
             notItems = [.contentAvailable]
         }
         let n = NotificationPusher(apnsTopic: notificationsAppId)
-
+        print("gönderilen notification: \(notItems)")
         n.pushAPNS(
             configurationName: notificationsAppId,
             deviceTokens: deviceTokens,
             notificationItems: notItems) {
                 responses in
-                print("\(responses)")
+                print("not response is: \(responses)")
         }
     }
 
